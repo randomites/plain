55571b97894821c661cb449faab8b73ea863c937
jzr <zarevucky.jiri@gmail.com>
2017-07-28 21:05:24 +0200

    Next iteration of the interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//! A small Rust library that allows users to reinterpret data of certain types safely.
+//! A small Rust library that allows users to interpret arrays of bytes
+//! as certain kinds of structures safely.
 //!
 //! This crate provides an unsafe trait [`Plain`](trait.Plain.html), which the user
 //! of the crate uses to mark types for which operations of this library are safe.
@@ -7,6 +8,18 @@
 //! Other than that, everything else in this crate is perfectly safe to use as long
 //! as the `Plain` trait is not implemented on inadmissible types (similar to how
 //! `Send` and `Sync` in the standard library work).
+//!
+//! # Purpose
+//! 
+//! In low level systems development, it is sometimes necessary to
+//! interpret locations in memory as data structures. Functions of
+//! this crate serve to avoid pitfalls associated with that, without
+//! having to resort to big, full-featured (de)serialization libraries.
+//!
+//! On the other hand, this crate contains no provisions when it comes
+//! to handling differences in encoding and byte ordering between
+//! platforms. As such, it is entirely unsuitable for processing
+//! external data such as file contents or network packets.
 //!
 //! # Examples
 //!
@@ -20,7 +33,7 @@
 //!
 //! extern crate plain;
 //! use plain::Plain;
-//!
+//! use std::mem;
 //!
 //!
 //! #[repr(C)]
@@ -45,22 +58,41 @@
 //! // SAFE: ELF64Header satisfies all the requirements of `Plain`.
 //! unsafe impl Plain for ELF64Header {}
 //!
-//! fn reinterpret_buffer(buf: &[u8]) -> &ELF64Header {
-//!     match plain::from_bytes(buf) {
-//!         Err(_) => panic!("The buffer is either too short or not aligned!"),
-//!         Ok(elfref) => elfref,
+//! impl ELF64Header {
+//! 	fn from_bytes(buf: &[u8]) -> &ELF64Header {
+//!			plain::from_bytes(buf).expect("The buffer is either too short or not aligned!")
+//!		}
+//!
+//!		fn from_mut_bytes(buf: &mut [u8]) -> &mut ELF64Header {
+//!			plain::from_mut_bytes(buf).expect("The buffer is either too short or not aligned!")
+//!		}
+//!
+//!		fn copy_from_bytes(buf: &[u8]) -> ELF64Header {
+//!			let mut h = ELF64Header::default();
+//!			h.copy_from_bytes(buf).expect("The buffer is too short!");
+//!			h
+//!		}
+//! }
+//! 
+//! # fn process_elf(elf: &ELF64Header) {}
+//!
+//! // Conditional copying for ultimate hackery.
+//! fn opportunistic_elf_processing(buf: &[u8]) {
+//! 	if plain::is_aligned::<ELF64Header>(buf) {
+//!         // No copy necessary.
+//!			let elf_ref = ELF64Header::from_bytes(buf);
+//!			process_elf(elf_ref);
+//!     } else {
+//!         // Not aligned properly, copy to stack first.
+//!			let elf = ELF64Header::copy_from_bytes(buf);
+//!			process_elf(&elf);
 //!     }
 //! }
 //!
-//! fn copy_from_buffer(buf: &[u8]) -> ELF64Header {
-//!     let mut h = ELF64Header::default();
-//!     h.as_mut_bytes().copy_from_slice(buf);
-//!     h
-//! }
-//!
 //! #[repr(C)]
+//! #[derive(Default, Copy, Clone)]
 //! struct ArrayEntry {
-//!     pub name: [u8; 64],
+//!     pub name: [u8; 32],
 //!     pub tag: u32,
 //!     pub score: u32,
 //! }
@@ -76,6 +108,13 @@
 //!         Err(_) => panic!("The buffer is not aligned!"),
 //!         Ok(arr) => arr,
 //!     }
+//! }
+//!
+//! fn array_from_unaligned_bytes(buf: &[u8]) -> Vec<ArrayEntry> {
+//!		let length = buf.len() / mem::size_of::<ArrayEntry>();
+//! 	let mut result = vec![ArrayEntry::default(); length];
+//!  	(&mut result).copy_from_bytes(buf).expect("Cannot fail here.");
+//!		result
 //! }
 //!
 //! # fn main() {}
@@ -101,293 +140,27 @@
 
 #![no_std]
 
+mod error;
+pub use error::Error;
+
+mod plain;
+pub use plain::Plain;
+
+mod methods;
+pub use methods::{
+	is_aligned,
+	from_bytes,
+	slice_from_bytes,
+	slice_from_bytes_len,
+	from_mut_bytes,
+	slice_from_mut_bytes,
+	slice_from_mut_bytes_len,
+	copy_from_bytes,
+};
+
 #[cfg(test)]
 #[macro_use]
 extern crate std;
-
-use core::{mem, slice};
-
-/// A trait for plain reinterpretable data.
-///
-/// A type can be [`Plain`](trait.Plain.html) if it is `#repr(C)` and only contains
-/// data with no possible invalid values. Specifically,
-/// `bool`, `char`, `enum`s, tuples, pointers and references are not
-/// `Plain`. On the other hand, arrays of a `Plain` type, and
-/// structures where all members are plain, are usually okay.
-///
-/// On top of this, implicit padding may technically be uninitialized bytes,
-/// therefore reading them might constitute undefined behavior by definition.
-/// As such, you currently must not apply `Plain` to structures with implicit
-/// padding. I.e. the size of the whole struct (as returned by `mem::size_of`)
-/// must be equal to the sum of sizes of its fields. The easiest way to assure
-/// this is learning padding rules for `#repr(C)` and explicitly provide
-/// padding as dummy fields where appropriate.
-///
-/// All methods of this trait are implemented automatically as wrappers
-/// for crate-level funtions.
-///
-pub unsafe trait Plain {
-    #[inline]
-    fn from_bytes(bytes: &[u8]) -> Result<&Self, Error>
-        where Self: Sized
-    {
-        self::from_bytes(bytes)
-    }
-
-    #[inline]
-    fn slice_from_bytes(bytes: &[u8]) -> Result<&[Self], Error>
-        where Self: Sized
-    {
-        self::slice_from_bytes(bytes)
-    }
-
-    #[inline]
-    fn slice_from_bytes_len(bytes: &[u8], len: usize) -> Result<&[Self], Error>
-        where Self: Sized
-    {
-        self::slice_from_bytes_len(bytes, len)
-    }
-
-    #[inline]
-    fn from_mut_bytes(bytes: &mut [u8]) -> Result<&mut Self, Error>
-        where Self: Sized
-    {
-        self::from_mut_bytes(bytes)
-    }
-
-    #[inline]
-    fn slice_from_mut_bytes(bytes: &mut [u8]) -> Result<&mut [Self], Error>
-        where Self: Sized
-    {
-        self::slice_from_mut_bytes(bytes)
-    }
-
-    #[inline]
-    fn slice_from_mut_bytes_len(bytes: &mut [u8], len: usize) -> Result<&mut [Self], Error>
-        where Self: Sized
-    {
-        self::slice_from_mut_bytes_len(bytes, len)
-    }
-
-    #[inline(always)]
-    fn as_bytes(&self) -> &[u8] {
-        self::as_bytes(self)
-    }
-
-    #[inline(always)]
-    fn as_mut_bytes(&mut self) -> &mut [u8] {
-        self::as_mut_bytes(self)
-    }
-}
-
-unsafe impl Plain for u8 {}
-unsafe impl Plain for u16 {}
-unsafe impl Plain for u32 {}
-unsafe impl Plain for u64 {}
-unsafe impl Plain for usize {}
-
-unsafe impl Plain for i8 {}
-unsafe impl Plain for i16 {}
-unsafe impl Plain for i32 {}
-unsafe impl Plain for i64 {}
-unsafe impl Plain for isize {}
-
-unsafe impl Plain for f32 {}
-unsafe impl Plain for f64 {}
-
-unsafe impl<S> Plain for [S] where S: Plain {}
-
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum Error {
-    TooShort,
-    BadAlignment,
-}
-
-#[inline(always)]
-fn check_instance_size<T>(bytes: &[u8]) -> Result<(), Error> {
-    if bytes.len() < mem::size_of::<T>() {
-        // slice is too short for target type
-        Err(Error::TooShort)
-    } else {
-        Ok(())
-    }
-}
-
-#[inline(always)]
-fn check_slice_size<T>(bytes: &[u8], len: usize) -> Result<(), Error> {
-    if bytes.len() < len * mem::size_of::<T>() {
-        Err(Error::TooShort)
-    } else {
-        Ok(())
-    }
-}
-
-#[inline(always)]
-fn check_alignment<T>(bytes: &[u8]) -> Result<(), Error> {
-    let align_offset = (bytes.as_ptr() as usize) % mem::align_of::<T>();
-
-    if align_offset != 0 {
-        // badly aligned slice
-        Err(Error::BadAlignment)
-    } else {
-        Ok(())
-    }
-}
-
-/// Safely converts a reference to an immutable
-/// byte slice of appropriate length.
-///
-/// This function cannot fail.
-///
-#[inline]
-pub fn as_bytes<S>(s: &S) -> &[u8]
-    where S: Plain + ?Sized
-{
-    // Even though slices can't under normal circumstances be cast
-    // to pointers, here in generic code it works.
-    // This means that `s` can be a slice or a regular reference,
-    // at the caller's discretion.
-    let bptr = s as *const S as *const u8;
-    let bsize = mem::size_of_val(s);
-    unsafe { slice::from_raw_parts(bptr, bsize) }
-}
-
-/// Safely converts a reference to a mutable
-/// byte slice of appropriate length.
-///
-/// This function cannot fail.
-///
-#[inline]
-pub fn as_mut_bytes<S>(s: &mut S) -> &mut [u8]
-    where S: Plain + ?Sized
-{
-    let bptr = s as *mut S as *mut u8;
-    let bsize = mem::size_of_val(s);
-    unsafe { slice::from_raw_parts_mut(bptr, bsize) }
-}
-
-/// Safely converts a byte slice to a reference.
-///
-/// However, if the byte slice is not long enough
-/// to contain target type, or if it doesn't
-/// satisfy the type's alignment requirements,
-/// the function returns an error.
-///
-/// However, the function will not fail when the
-/// byte slice is longer than necessary, since it is
-/// a common practice to interpret the beginning of
-/// a slice as a fixed-size header.
-///
-/// In most cases it's preferrable to allocate
-/// a value/slice of the target type and use
-/// [`as_mut_bytes()`](fn.as_mut_bytes.html) to copy
-/// data instead. That way, any issues with alignment
-/// are implicitly avoided.
-///
-#[inline]
-pub fn from_bytes<T>(bytes: &[u8]) -> Result<&T, Error>
-    where T: Plain
-{
-    try!(check_instance_size::<T>(bytes));
-    try!(check_alignment::<T>(bytes));
-    Ok(unsafe { &*(bytes.as_ptr() as *const T) })
-}
-
-/// Similar to [`from_bytes()`](fn.from_bytes.html),
-/// except that the output is a slice of T, instead
-/// of a reference to a single T. All concerns about
-/// alignment also apply here, but size is handled
-/// differently.
-///
-/// The result slice's length is set to be
-/// `bytes.len() / size_of::<T>()`, and there
-/// are no requirements for input size. I.e.
-/// the result may be empty slice, and the input
-/// slice doesn't necessarily have to end on `T`'s
-/// boundary. The latter has pragmatic reasons: If the
-/// length of the array is not known in advance,
-/// e.g. if it's terminated by a special element,
-/// it's perfectly legal to turn the whole rest
-/// of data into `&[T]` and set the proper length
-/// after inspecting the array.
-///
-/// In most cases it's preferrable to allocate
-/// a value/slice of the target type and use
-/// [`as_mut_bytes()`](fn.as_mut_bytes.html) to copy
-/// data instead. That way, any issues with alignment
-/// are implicitly avoided.
-///
-/// ## Example
-///
-/// ```rust,should_panic
-/// use plain::Plain;
-/// let bytes = &[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
-/// let nums: &[u32] = u32::slice_from_bytes(bytes).unwrap();
-/// // Oops! This `unwrap()` will actually panic, in some cases!
-/// // The byte slice is NOT aligned! Don't write code like this!
-///
-/// // If the above doesn't panic, this holds:
-/// assert_eq!(nums.len(), 3);
-/// # assert!(false);
-/// ```
-#[inline]
-pub fn slice_from_bytes<T>(bytes: &[u8]) -> Result<&[T], Error>
-    where T: Plain
-{
-    let len = bytes.len() / mem::size_of::<T>();
-    slice_from_bytes_len(bytes, len)
-}
-
-
-/// Same as [`slice_from_bytes()`](fn.slice_from_bytes.html),
-/// except that it takes explicit length of the result slice.
-///
-/// If the input slice can't satisfy the length, returns error.
-/// The input slice is allowed to be longer than necessary.
-#[inline]
-pub fn slice_from_bytes_len<T>(bytes: &[u8], len: usize) -> Result<&[T], Error>
-    where T: Plain
-{
-    try!(check_alignment::<T>(bytes));
-    try!(check_slice_size::<T>(bytes, len));
-    Ok(unsafe { slice::from_raw_parts(bytes.as_ptr() as *const T, len) })
-}
-
-/// See [`from_bytes()`](fn.from_bytes.html).
-///
-/// Does the same, except with mutable references.
-#[inline]
-pub fn from_mut_bytes<T>(bytes: &mut [u8]) -> Result<&mut T, Error>
-    where T: Plain
-{
-    try!(check_instance_size::<T>(bytes));
-    try!(check_alignment::<T>(bytes));
-    Ok(unsafe { &mut *(bytes.as_mut_ptr() as *mut T) })
-}
-
-/// See [`slice_from_bytes()`](fn.slice_from_bytes.html).
-///
-/// Does the same, except with mutable references.
-#[inline]
-pub fn slice_from_mut_bytes<T>(bytes: &mut [u8]) -> Result<&mut [T], Error>
-    where T: Plain
-{
-    let len = bytes.len() / mem::size_of::<T>();
-    slice_from_mut_bytes_len(bytes, len)
-}
-
-/// See [`slice_from_bytes_len()`](fn.slice_from_bytes_len.html).
-///
-/// Does the same, except with mutable references.
-#[inline]
-pub fn slice_from_mut_bytes_len<T>(bytes: &mut [u8], len: usize) -> Result<&mut [T], Error>
-    where T: Plain
-{
-    try!(check_alignment::<T>(bytes));
-    try!(check_slice_size::<T>(bytes, len));
-    Ok(unsafe { slice::from_raw_parts_mut(bytes.as_ptr() as *mut T, len) })
-}
 
 #[cfg(test)]
 mod tests;
--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,12 @@
 
-use super::*;
+
+#![allow(dead_code)]
+
+use ::*;
 use core::mem;
 
 #[repr(C)]
-#[derive(Debug, Copy, Eq, Clone, PartialEq)]
+#[derive(Debug, Default, Copy, Eq, Clone, PartialEq)]
 struct Dummy1 {
     field1: u64,
     field2: u32,
@@ -15,7 +18,7 @@ struct Dummy1 {
 unsafe impl Plain for Dummy1 {}
 
 #[repr(C)]
-#[derive(Debug, Copy, Eq, Clone, PartialEq)]
+#[derive(Debug, Default, Copy, Eq, Clone, PartialEq)]
 struct Dummy2 {
     field1: u8,
     field2: u8,
@@ -26,20 +29,20 @@ struct Dummy2 {
 
 unsafe impl Plain for Dummy2 {}
 
+fn as_bytes<T: ?Sized>(r: &T) -> &[u8] {
+	unsafe { methods::as_bytes(r) }
+}
+
+fn as_mut_bytes<T: Plain + ?Sized>(r: &mut T) -> &mut [u8] {
+	unsafe { methods::as_mut_bytes(r) }
+}
+
 #[test]
 fn one_too_short() {
     let b = vec![0u8; mem::size_of::<Dummy1>()-1];
 
     let r = Dummy1::from_bytes(&b);
     assert!(r == Err(Error::TooShort));
-}
-
-#[test]
-fn well_aligned() {
-    let b = vec![0u8; mem::size_of::<Dummy1>()+1];
-
-    // No failure.
-    Dummy1::from_bytes(&b).unwrap();
 }
 
 #[test]
@@ -52,6 +55,30 @@ fn unaligned() {
 }
 
 #[test]
+fn copy_test() {
+    let t1 = Dummy1 {
+        field1: 0xaaaaaaaaaaaaaaaau64,
+        field2: 0xbbbbbbbbu32,
+        field3: 0xccccu16,
+        field4: 0xddu8,
+        field5: 0xeeu8,
+    };
+
+    let mut t2 = Dummy2::default();
+
+    assert!(t2.copy_from_bytes(as_bytes(&t1)) == Ok(()));
+
+    assert!(t2.field1 == 0xaau8);
+    assert!(t2.field2 == 0xaau8);
+    assert!(t2.field3 == 0xaaaau16);
+    assert!(t2.field4 == 0xaaaaaaaau32);
+    assert!(t2.field5 == 0xbbbbbbbbccccddeeu64 || t2.field5 == 0xeeddccccbbbbbbbbu64);
+
+    let sz = mem::size_of::<Dummy2>();
+    assert!(t2.copy_from_bytes(&as_bytes(&t1)[..sz-1]) == Err(Error::TooShort));
+}
+
+#[test]
 fn basic_function() {
     let t1 = Dummy1 {
         field1: 0xaaaaaaaaaaaaaaaau64,
@@ -61,7 +88,7 @@ fn basic_function() {
         field5: 0xeeu8,
     };
 
-    let r1: &Dummy2 = from_bytes(t1.as_bytes()).unwrap();
+    let r1: &Dummy2 = from_bytes(as_bytes(&t1)).unwrap();
 
     assert!(r1.field1 == 0xaau8);
     assert!(r1.field2 == 0xaau8);
@@ -69,19 +96,28 @@ fn basic_function() {
     assert!(r1.field4 == 0xaaaaaaaau32);
     assert!(r1.field5 == 0xbbbbbbbbccccddeeu64 || r1.field5 == 0xeeddccccbbbbbbbbu64);
 
-    let r2 = r1.as_bytes();
+    let r2 = as_bytes(r1);
     assert!(r2.len() == mem::size_of::<Dummy1>());
     assert!(r2[5] == 0xaa);
 
-    // nop
-    let r3 = r2.as_bytes();
+    let size = r2.len();
+    let r3 = as_bytes(r2);
+    assert!(r3.len() == size);
 
     let r4 = Dummy1::from_bytes(r3).unwrap();
 
-    let r5 = from_bytes::<Dummy2>(r4.as_bytes()).unwrap();
+    let r5 = from_bytes::<Dummy2>(as_bytes(r4)).unwrap();
 
-    let r6 = slice_from_bytes::<Dummy1>(r5.as_bytes()).unwrap();
+	{
+		let r6 = slice_from_bytes::<Dummy1>(as_bytes(r5)).unwrap();
 
-    assert!(r6.len() == 1);
-    assert!(t1 == r6[0]);
+		assert!(r6.len() == 1);
+		assert!(t1 == r6[0]);
+	}
+
+	let r7 = slice_from_bytes::<u64>(as_bytes(r5)).unwrap();
+	assert!(r7.len() == 2);
+
+	assert!(r7[0] == 0xaaaaaaaaaaaaaaaau64);
+	assert!(r7[1] == 0xbbbbbbbbccccddeeu64 || r7[1] == 0xeeddccccbbbbbbbbu64);
 }
